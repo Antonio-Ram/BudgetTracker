317d79981279d5cfa736c2c4379cb0838e620418
Antonio <tonito@umich.edu>
2022-02-22 10:48:11 -0500

    not adding saved info

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,7 +13,7 @@ request.onsuccess = function(event) {
     db = event.target.result;
     // check to see if app is online, then run uploadBudget() to send data
     if (navigator.onLine) {
-        //uploadBudget();
+        uploadBudget();
     }
 };
 
@@ -31,3 +31,41 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    //get all records
+    const getAll = budgetObjectStore.getAll();
+    // when successful, run this
+    getAll.onsuccess = function() {
+        if (getAll.result.lengeth > 0) {
+            fetch('/api', {
+                method: 'POST',
+                body: JSON.stringify.apply(getAll.result),
+                headers: {
+                    Accept: 'application/json, test/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_budget'], 'readwrite');
+                //accss the new_budget object store
+                const budgetObjectStore = transaction.objectStore('new_budget');
+                // clear all items in your store
+                budgetObjectStore.clear();
+
+                alert('All saved budgets have been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+}
+
+window.addEventListener('online', uploadBudget);
